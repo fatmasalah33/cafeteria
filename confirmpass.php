19d26c8857e33d0b49b123cb6ed510895504666b
Abdallah Mohsen <abdallahmohsen979@gmail.com>
2022-07-01 17:24:05 +0200

    abdallah commit

--- confirmpass.php
+++ confirmpass.php
@@ -2,8 +2,19 @@
 
 require "connection.php";  
 session_start();
-$updatepass=$connection->prepare("UPDATE  {$_SESSION ["table_update"]} SET password=?  WHERE id=? ");
-$updatepass->execute([ $_POST ["password"],$_SESSION ["id_update"] ]);
- echo $_SESSION["table_update"] , $_POST["password"] ,$_SESSION["id_update"]; 
- header("Location: login.php");
+$password=$_POST['password'];
+$confpassword=$_POST['conf-password'];
+if ($password==$confpassword)
+{
+    $updatepass=$connection->prepare("UPDATE  {$_SESSION ["table_update"]} SET password=?  WHERE id=? ");
+    $updatepass->execute([ $_POST ["password"],$_SESSION ["id_update"] ]);
+    echo $_SESSION["table_update"] , $_POST["password"] ,$_SESSION["id_update"]; 
+    header("Location: login.php");
+}
+else
+{
+    //echo " The Password and Confirm Password is not the same";
+    header("Location: updatepassword.php");
+}
+
 ?>
