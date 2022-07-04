3e4ef1671d169acf715a92d9907d7cc12fc71f2e
fatma3333eg <fatma3333eg@gmail.com>
2022-07-04 21:56:43 +0200

    edit4 in admin home

--- setproductdb.php
+++ setproductdb.php
@@ -4,10 +4,11 @@ $username = "root";
 $password = "";
 $dbname= "cafeteria";
 $arr=$_POST; 
-$array1 = array_chunk($arr, 3);
-$totalPrice=$array1[0][0];
-$notes=$array1[0][1];
-$roomNum=$array1[0][2];
+$array1 = array_slice($arr, 0, 3);
+// var_dump($array1);
+$totalPrice=$array1["price"];
+$notes=$array1["notes"];
+$roomNum=$array1["noroom"];
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname); 
 $id=$_GET["id"];
@@ -19,12 +20,16 @@ if ($conn->query($sql) === TRUE) {
 }
 // var_dump($last_id);
 
+$array2=array_slice($arr, 3);
+$array2 = array_chunk($array2, 1);
+var_dump($array2[1]);
 
- foreach($array1[1] as $item){
+for ($i=0;$i<count($array2);$i++){
+ foreach($array2[$i] as $item){
     
     $cleanData = json_decode($item); 
- 
-    // echo $cleanData->name; 
+
+    echo $cleanData->name; 
     $query = "select id from products where name='$cleanData->name' "; 
 
     $row=$conn->query($query) ; 
@@ -39,7 +44,8 @@ if ($conn->query($sql) === TRUE) {
     
 
 
- }
+  }
+}
  header("Location:home.php");
 
 ?>
