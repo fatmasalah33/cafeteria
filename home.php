127b5e6a85bc241f91c63ea283577cca3a974652
fatma3333eg <fatma3333eg@gmail.com>
2022-07-04 17:11:31 +0200

    edit user home page

--- home.php
+++ home.php
@@ -1,6 +1,7 @@
 <html>
 
 <head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="css/style.css">
   <script src="js/jquery-3.5.1.min.js"></script>
@@ -12,241 +13,188 @@
 
 <body>
 <?php
-   
     
     require 'connection.php';?>
 <div class="container">
     <div class="row">
-        <div class="col-md-3" >
-            <form action=<?="setproductdb.php?id=2"?> method="post" onsubmit="setorder()">
-        <!-- <input type="hidden" name="pricex" value="" id="test"> --> 
-          <input type="submit" value="submit"/>
-        </form>
         
+        <div class="col-md-3" >
+       
           <div id="myDIV">
 
           </div>
-          <!-- <input type="text" name="qty" id="qty" oninput="myFunction()"> -->
-          <span id="total price"> </span>
-       
-          <!-- <button>-</button> -->
-           <div id="demo"></div>
+          <form action=<?="setproductdb.php?id=2"?> method="post" onsubmit="setorder()">
+        <input type="hidden" name="price" value="" id="totalprice">
+        <input type="text" name="notes" placeholder="notes" ><br>
+
+        <select name="noroom" id="roomno">
+        <?php
+							$queryString=$connection->prepare('SELECT DISTINCT room_no FROM users');
+							$queryString->execute();
+							$users=$queryString->fetchAll();
+								foreach ($users as $user){?>
+            <option value="<?= $user['room_no']?>"><?= $user['room_no']?></option><?php }?>
+            </div>
+        </select>
+        <input type="submit" value="confirm">
+        </form>
+        <span id="tottal"></span>
         </div>
         <div class="col-md-9" >
             <div class="container">
                 <div class="row">
                 <?php
-                
 							$queryString=$connection->prepare('SELECT * FROM products');
 							$queryString->execute();
 							$products=$queryString->fetchAll();
 								foreach ($products as $product){
                                     $myId=$product['id'];
                                     $productName=$product['name'];
-                                    $productPrice=$product['price'];
-                            ?> 
+                                    $productPrice=$product['price'];?>
                     <div class="col-md-4">
                     <div class="card" style="width: 18rem;">
-  <img  class="myimg"  onclick="getDetails(<?= $myId ?>,'<?= $productName ?>',<?=$productPrice?>,event)" src="<?='prouductphotos/'.$product['img']?>" class="card-img-top" alt="...">
+  <img onclick="getDetails(<?= $myId ?>,'<?= $productName ?>',<?=$productPrice?>,event)"  id="<?= $myId ?>"   src="<?='prouductphotos/'.$product['img']?>" class="card-img-top" alt="...">
   <div class="card-body">
     <p class="card-text"><?= $product['name']?></p>
 <p><?= $product['price']?></p>  
 </div>
 </div>
+                        
                     </div><?php }?>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<script> 
-let img_id;
-    function  getDetails(id,name,price,event){  
-      // console.log(event.target.onclick);
-      event.target.onclick=null;   
-      // console.log(event.target.onclick);
-      //   console.log(event.target,id,name,price);  
-      div_container= document.createElement("div.container");
-     img_id=id;
-      prod_name=document.createElement("span.productName"); 
-      prod_name.innerHTML=name;
-      prod_qty=document.createElement("input"); 
-       prod_qty.type="number";  
-       prod_qty.value=1;
-    //   prod_qty.type="number"; 
-      total_prod_price=document.createElement("span.total_prod_price");   
-      total_prod_price.innerHTML=price;
-      div_container.append( prod_name);
-      div_container.append( prod_qty);
-      div_container.append( total_prod_price);
-      document.getElementById("myDIV").append(div_container); 
-      prod_qty.addEventListener("change",function(e){ 
-        // console.log(e,e.target.nextSibling);
-        e.target.nextSibling.innerHTML=price*e.target.value; 
-      }) 
+<script>
 
+   let total=0
+  var i=0
+   var z=0
+   let sum=0
+   var arrtotal=[]
+var arryproductName=[]
+    function getDetails(id,productName,productPrice,event){
+        if(arryproductName.includes(productName)){
+                
+                alert('The item is already in the order')
+      
+            }else{
+                arryproductName.push(productName)
+            
+       
 
-    }  
+        var para5=document.createElement("div");
+        para5.setAttribute('class','cont')
+        let para = document.createElement("span")
+        para.innerText =productName;
+        
+        para5.appendChild(para);
+        let para3 = document.createElement("input");
+        para3.value =1
+        para3.type="number"
+        para3.min="1"
+        para3.max="5"
+        para3.onchange=(e)=>{
+            // console.log(e.target.value) 
+            // console.log(e.target.previousSibling)
+            // console.log(e.target.nextSibling)
+            e.target.nextSibling.innerHTML=productPrice*e.target.value
+            total=0
+           z=productPrice*e.target.value
+        //    console.log(z)
+           total+=z
+        //    console.log(total)
+           arrtotal[productName]=total
+        //    console.log(arrtotal)
+        //  sum += arrtotal.reduce((a, b) => a + b, 0);
+sum=0;
+         for (var key in arrtotal) {
+    sum+=arrtotal[key]
+    document.getElementById('totalprice').value=sum;
+    
+    document.getElementById('tottal').innerHTML=sum;
+  
+}
+console.log(sum)
+        }
+       para5.appendChild(para3);
+       para2 = document.createElement("span");
+       para2.innerHTML=productPrice;
+       arrtotal[productName]=productPrice
+       sum += productPrice
+       document.getElementById('totalprice').value=sum;
+       console.log(sum)
+       document.getElementById('tottal').innerHTML=sum;
+        // console.log(arrtotal)
+       para5.appendChild(para2);
+   
+
+       let para7 = document.createElement("i");
+       para7.setAttribute("class","fa-solid fa-xmark")
+       para5.appendChild(para7);
+       para7.onclick=(e)=>{
+        // console.log(e.target.parentElement)
+        e.target.parentElement.style.display="none"
+        // console.log(e.target.parentElement.firstChild.innerHTML)
+        var theproductName=e.target.parentElement.firstChild.innerHTML;
+        console.log(theproductName)
+       
+    delete arrtotal[theproductName]
+    var b = 0;
+  while (b< arryproductName.length) {
+    if (arryproductName[b] === theproductName) {
+        arryproductName.splice(b, 1);
+    } else {
+      ++b;
+    }
+  }
+    //  arryproductName.remove("Tea")
+    console.log(arryproductName)
+    console.log(arrtotal)
+    sum=0;
+    
+         for (var key in arrtotal) {
+    sum+=arrtotal[key]
+    document.getElementById('totalprice').value=sum;
+    
+    document.getElementById('tottal').innerHTML=sum;
+}
+console.log(sum)
+    }
+  
+       document.getElementById("myDIV").appendChild(para5);
+
+       
+    }
+
+         
+    }
+ 
+
     function setorder(){
   
-        let prod_arr=[]; 
-         total_price=0;
-   containr_arr= document.getElementsByTagName("div.container")
-   for(i=0;i<containr_arr.length;i++){ 
-    prod_name=containr_arr[i].children[0].innerHTML;
-    prod_quantity=containr_arr[i].children[1].value;
-    prod_arr.push({
-        name:prod_name,
-        quantity:prod_quantity
-    }); 
-   total_price+=Number(containr_arr[i].children[2].innerHTML);
-    
-    }   
-    console.log(total_price) ;
-   for(i=0;i<prod_arr.length;i++){
-    ipt1=document.createElement("input"); 
-    ipt1.name=prod_arr[i].name;
-    ipt1.type="hidden";
-    ipt1.value=JSON.stringify(prod_arr[i]); 
-    document.forms[0].append(ipt1)
-   }
-   }
+  let prod_arr=[]; 
+   total_price=0;
+containr_arr= document.getElementsByClassName("cont")
+for(i=0;i<containr_arr.length;i++){ 
+prod_name=containr_arr[i].children[0].innerHTML;
+prod_quantity=containr_arr[i].children[1].value;
+prod_arr.push({
+  name:prod_name,
+  quantity:prod_quantity
+}); 
 
-  
-// //    function test(e){
-// //     // #1:nth-child(1)
-// // var thediv =document.getElementById(`${i}`)
-// // console.log(thediv)
-// //     // var x = document.querySelector(`#myDIV #${i} input`).value;
-// //      var x=thediv.querySelector("input").value
-// //     x++;
-// //     thediv.querySelector("input").value=x
-   
-// //         para2.innerText = (document.getElementById("test").value)*thediv.querySelector("input").value
-// //         // para2.setAttribute("id", productPrice)
+}   
 
-    
-// //    }
-// //    i=0;
-// //    var para2;
-// //    var productList = [];
-// //    var product ;
-// //    let para4;
-// //    let total=0
-// //    var z=0
-// //    var arrtotal=[]
-// //     function getDetails(id,productName,productPrice,event){
-// //         console.log(event)
-//         // if(arrtotal[productName]){
-//         //     console.log("hi")
-//         // }
-// //          product = {
-// //     id: id,
-// //     name: productName,
-// //      price:productPrice
-// //   };
- 
-// //   productList.push(product);
-//   var para5=document.createElement("div");
-// //   para5.setAttribute("class","outerdiv")
- 
-//         let para = document.createElement("span")
-     
-//         para.innerText =productName;
-//         para5.appendChild(para);
-//         let para3 = document.createElement("input");
-//         para3.value =1
-//         para3.type="number"
-//         para3.min="1"
-//         para3.max="5"
-//         para3.onchange=(e)=>{
-//             // console.log(e.target.value) 
-//             // console.log(e.target.previousSibling)
-//             // console.log(e.target.nextSibling)
-//             e.target.nextSibling.innerHTML=productPrice*e.target.value
-//             total=0
-//            z=productPrice*e.target.value
-//            console.log(z)
-//            total+=z
-//            console.log(total)
-//            arrtotal[productName]=total
-//            console.log(arrtotal)
-
-//         }
-        
-        
-      
-//         // para3.setAttribute("oninput", "myFunction(5)")
-//        para5.appendChild(para3);
-     
-//         // para4= document.createElement("button")
-//         // // para4.setAttribute("id",productList[i].id)
-//         // para4.onclick=(e)=>{
-//         //     console.log(e.target.previousSibling.value)
-//         //    var qty= e.target.previousSibling.value
-//         //    qty++
-//         //    e.target.previousSibling.value=qty
-//         //    console.log(productPrice*qty)
-//         //    e.target.nextSibling.innerHTML=productPrice*qty
-
-//         // }
-//         // // para4.setAttribute("onclick", "test()")
-//         // para4.innerHTML ="+"
-//         // // para4.setAttribute("id", productList[i].id)
-//         // para5.appendChild(para4);
-//         // para6= document.createElement("button")
-//         // // para4.setAttribute("id",productList[i].id)
-//         // para6.onclick=(e)=>{
-//         //     console.log(e.target.previousSibling.value)
-//         //    var qty2= e.target.previousSibling.value
-//         //    qty2--
-//         //    e.target.previousSibling.value=qty2
-//         // //    console.log(productPrice*qty)
-//         // //    e.target.nextSibling.innerHTML=productPrice*qty
-
-//         // }
-       
-//         // para6.innerHTML ="-"
-//         // // para4.setAttribute("id", productList[i].id)
-//         // para5.appendChild(para6);
-      
-//          para2 = document.createElement("span");
-//         para2.innerHTML=productPrice;
-//         arrtotal[productName]=productPrice
-//         // console.log(arrtotal)
-//        para5.appendChild(para2);
-//        para5.setAttribute("id",i)
-//         document.getElementById("myDIV").appendChild(para5);
-  
-//         i++;
-//     // i++
-//         // productList = [];
-
-//         // document.getElementById("test").value=productPrice
-//         // console.log(productList)
-//         // localStorage.setItem('ourproduct', JSON.stringify(productList));
-        
-       
-
-//          console.log(arrtotal)
-         
-//     }
-//     //  , {once : true});
-
-   
-//     // console.log(productList)
-// //     var List;
-// // List = JSON.parse(localStorage.getItem("ourproduct"));
-// //     console.log(List)
-// //  function x(){
-
-// //  }
-// //     function myFunction(productPrice) {
-// //   var x = document.getElementsByTagName("input")[0].value;
-// //   console.log()
-// //   document.getElementById("demo").innerHTML = "mush: " + x*productPrice ;
-// // }
-
+for(i=0;i<prod_arr.length;i++){
+ipt1=document.createElement("input"); 
+ipt1.name=prod_arr[i].name;
+ipt1.type="hidden";
+ipt1.value=JSON.stringify(prod_arr[i]); 
+document.forms[0].append(ipt1)
+}
+}
     
 
 </script>
--- setproductdb.php
+++ setproductdb.php
@@ -3,45 +3,43 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname= "cafeteria";
-
+$arr=$_POST; 
+$array1 = array_chunk($arr, 3);
+$totalPrice=$array1[0][0];
+$notes=$array1[0][1];
+$roomNum=$array1[0][2];
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname); 
 $id=$_GET["id"];
-$sql = "INSERT into orders (user_id,status,total_price,room_no,notes)
-           values($id,'processing',1900,6,'no notes') ";
+$sql = "INSERT into orders (user_id,total_price,room_no,notes) values($id,$totalPrice,$roomNum,'$notes') ";
 
 if ($conn->query($sql) === TRUE) {
   $last_id = $conn->insert_id; 
-  var_dump($last_id);
+  
 }
-// require 'connection.php'; 
-var_dump($_GET["id"]); 
+// var_dump($last_id);
 
-// $order_insert=$connection->prepare('INSERT into orders (user_id,status,total_price,room_no,notes) values(?,"processing",1900,6,"no notes") ');
-// $order_insert->execute([$_GET["id"]]);  
-// $stmt = $connection->prepare("...");
-// $stmt->execute();
-// $id = $connection->lastInsertId();
-// var_dump($id);
-$arr=$_POST;  
-echo"<pre>";
-//  var_dump($arr); 
- echo"</pre>"; 
- foreach($arr as $item){
-    $tempData = str_replace("\\", "",$item);
+
+ foreach($array1[1] as $item){
+    
     $cleanData = json_decode($item); 
+ 
     // echo $cleanData->name; 
     $query = "select id from products where name='$cleanData->name' "; 
 
     $row=$conn->query($query) ; 
     $prod_id=$row->fetch_assoc();
-    // echo $cleanData->quantity ;   
-    echo $prod_id["id"]  ;  
-    // $sql = "INSERT into order_details( ) values(5,3,2)";
-     // $product_insert=$connection->prepare('INSERT into order_details values(5,3,2)');
-     // $product_insert->execute();  
+    $qty= $cleanData->quantity ;   
+    $idprod= $prod_id["id"]  ; 
+
+    $query1 = "INSERT into order_details(product_id,order_id,qty  ) values($idprod,$last_id,$qty)";
+
+    $conn->query($query1);
+      
+    
 
 
  }
+ header("Location:home.php");
 
 ?>
