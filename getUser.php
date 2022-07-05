4c5f446c2721cb58b248539dcd19b9f0272a65e6
fatma3333eg <fatma3333eg@gmail.com>
2022-07-05 17:02:25 +0200

    edit in checks page

--- getUser.php
+++ getUser.php
@@ -8,6 +8,10 @@ if($mysqli->connect_error) {
 $idUser=$_GET['q'];
 // $sql = "SELECT *
 // FROM users WHERE id = ?";
+
+if(!empty($idUser)){
+
+
 $sql="SELECT  users.name ,sum(orders.total_price) FROM users,orders WHERE users.id=orders.user_id and users.id=? GROUP by users.id;";
 $stmt = $mysqli->prepare($sql);
 $stmt->bind_param("s", $_GET['q']);
@@ -32,5 +36,30 @@ echo "</tr>";
 
 
 echo "</table>";
+}else{
+  $query="SELECT users.id, users.name ,sum(orders.total_price) FROM users,orders WHERE users.id=orders.user_id  GROUP by users.id;";
+  $row=$mysqli->query($query) ; 
+  $orderdetails=$row->fetch_all();
+    // var_dump($orderdetails[0]);
+    echo "<table border='2'>";
+    echo "<tr>";
+    echo "<th>order_date </th>";
+    echo "<th>total_price</th>";
+    echo "</tr>";
+    
+    foreach($orderdetails as $order ){
+      $id=$order[0];
+      echo "<tr>";
+  echo "<td><button onclick='showOrder($id,event)'  >+</button>" . $order[1]. "</td>";
+  echo "<td>" .$order[2]. "</td>";
+  echo "</tr>";
+  
+    }
+
+
+
+echo "</table>";
+
+}
 ?>
 
